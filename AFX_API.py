188a18204de0f7c4bea36e1fb0b3fc13a57c3795
collins-auto <120103749+collins-auto@users.noreply.github.com>
2022-12-23 17:33:39 +0100

    Update AFX_API.py

--- AFX_API.py
+++ AFX_API.py
@@ -9,7 +9,8 @@ def get_data(ticker, start, stop, interval="1D"):
     url = "https://afx.kwayisi.org/chart/ngx/" + ticker
     r = requests.get(url)
     js = r.text
-    
+
+# Format raw string retrieved from website for processing 
     bb = re.split("data:", js)[1]
     bb = bb[1:-9]
     bb = bb.split("],")
@@ -21,22 +22,31 @@ def get_data(ticker, start, stop, interval="1D"):
         bb[i] = bb[i].replace(r"(", "")
         bb[i] = bb[i].replace(r")", "")
         bb[i] = bb[i].replace(r'"', '')
-    
+   
+#Empty dictionary to contain stock price and date 
     data = {"Date": [],
             "Price": []}
-    
+
+#Append stock data to dictionary    
     for b in bb:
         data["Date"].append(b.split(",")[0])
         data["Price"].append(b.split(",")[1])
-        
+
+#Convert dictionary to dataframs        
     df = pd.DataFrame(data)
+
+#Change date from string to datetime object
     df["Date"] = pd.to_datetime(df["Date"])
     df.set_index("Date", inplace=True)
+
+#Change prices from string to float to enable mathematical operations 
     df["Price"] = df["Price"].str.replace(",", "")
     df["Price"] = df["Price"].astype(float)
-    
+
+#Resample dataframe to show prices for specified intervals (i.e. "1D", "1M", "1Y")    
     df = df.resample(interval).ffill()
     
+#Slice dataframe to present stock prices for specified dates
     df = df.loc[start:stop]
     
     print("Successful")
@@ -45,7 +55,7 @@ def get_data(ticker, start, stop, interval="1D"):
 
 
 
-
+#Get stock data for list of tickers
 def get_stocks(tickers, start, stop, interval="1D"):
     pf = pd.DataFrame()
     
@@ -55,7 +65,7 @@ def get_stocks(tickers, start, stop, interval="1D"):
     return pf
 
 
-
+#Get stocks with highest returns for a specific date period
 def highest_ngx_returns(ticker_list,top_no, start, stop):
 
     stock_returns = {}
@@ -63,17 +73,22 @@ def highest_ngx_returns(ticker_list,top_no, start, stop):
     for t in ticker_list:
         stock = get_data(t, start, stop)
         returns = stock.pct_change().dropna(how="all")
+
+#Calculate compounded annual growth rate
         CAGR = (1+returns).prod() ** (252 / returns.count()) - 1
 
         stock_returns[t] = CAGR[0]
-        
+
+#Sort returns from highest to lowest        
     stock_returns = sorted(stock_returns.items(), key=lambda x:x[1], reverse=True)
     stock_returns = dict(stock_returns)
+
+#List of top n performing stocks for the period
     top_list = [stock for stock in stock_returns][:top_no]
         
     return top_list
 
-
+#Get other details about stocks including Market Capitalization and sector
 def stock_info(ticker):
     url = "https://afx.kwayisi.org/ngx/" + ticker
     response = requests.get(url)
